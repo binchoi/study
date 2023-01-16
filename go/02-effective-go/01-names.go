52646145ec0b6346b9ee263dbcc13048d3580828
binchoi <binchoi16@gmail.com>
2023-01-16 20:01:56 +0900

    Add native go basic server

--- go/02-effective-go/01-names.go
+++ go/02-effective-go/01-names.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func unhex(c byte) byte {
+	switch {
+	case '0' <= c && c <= '9':
+		return c - '0'
+	case 'a' <= c && c <= 'f':
+		return c - 'a' + 10
+	case 'A' <= c && c <= 'F':
+		return c - 'A' + 10
+	}
+	return 0
+}
+
+func main() {
+	s := "12345"
+	for _, v := range s {
+		fmt.Println(unhex(byte(v)))
+	}
 }
